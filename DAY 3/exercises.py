fc63eb623322cea40e491fa43569b4d75dd265e9
asimmuj <asimmujtabahussain@gmail.com>
2026-08-24 13:48:36 +0530

    day-3

--- DAY 3/exercises.py
+++ DAY 3/exercises.py
@@ -10,7 +10,7 @@
 # checkeven(10)
 # checkeven(7)
 # checkeven(24)
-# checkeven(31)
+
 
 # def findmax(a,b):
 #     if a>b:
@@ -20,11 +20,11 @@
 # findmax(5,6)
 
 
-def count_passed(marks):
-    count=0
-    for mark in marks:
-        if mark>=40:
-            count+=1
-    print(count)
-marks = [35, 78, 91, 42, 67, 29, 88]
-count_passed(marks)
+# def count_passed(marks):
+#     count=0
+#     for mark in marks:
+#         if mark>=40:
+#             count+=1
+#     print(count)
+# marks = [35, 78, 91, 42, 67, 29, 88]
+# count_passed(marks)
