6dc5db711e5d47b85003ecf92d0727fd27b85d5a
asimmuj <asimmujtabahussain@gmail.com>
2026-08-31 18:49:49 +0530

    DAY-10

---
+++ DAY 10/exercises.py
@@ -0,0 +1,15 @@
+def binarysearch(numbers, target):
+    low =0
+    high= len(numbers)-1
+    while low<=high:
+        mid=(low+high)//2
+        if target==numbers[mid]:
+            return mid
+        elif target>numbers[mid]:
+            low=mid+1
+        else:
+            high=mid-1
+    return -1
+numbers = [5, 12, 18, 23, 31, 42, 57, 64, 78, 91]
+target=int(input("enter a number to search: "))
+print(binarysearch(numbers,target))
