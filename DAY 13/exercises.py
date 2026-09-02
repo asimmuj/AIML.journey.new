61219973bd3a984d1955d4b8488801e9d2776ae6
asimmuj <asimmujtabahussain@gmail.com>
2026-09-02 17:50:08 +0530

    DAY-13

---
+++ DAY 13/exercises.py
@@ -0,0 +1,24 @@
+numbers = [10, 20, 20, 20, 30, 40, 50]
+def first_occurrence(numbers, target):
+    low = 0
+    high = len(numbers) - 1
+    result = -1
+    while low <= high:
+        mid = (low + high) // 2
+        if numbers[mid] == target:
+            result = mid
+            # Search further left
+            high = mid - 1
+        elif target > numbers[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return result
+
+numbers = [10, 20, 20, 20, 30, 40, 50]
+target = int(input("Enter number: "))
+result = first_occurrence(numbers, target)
+if result != -1:
+    print("First occurrence:", result)
+else:
+    print("Number not found!")
