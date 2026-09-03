a11939eb04baf7f1d9c55965b027167f12fcc621
asimmuj <asimmujtabahussain@gmail.com>
2026-09-03 16:40:13 +0530

    DAY-14

--- DAY 14/exercises.py
+++ DAY 14/exercises.py
@@ -10,4 +10,15 @@
 # selection_sort(numbers)
 # print(numbers)
 
+# def selection_sort(numbers):
+#     for i in range(len(numbers)):
+#         min_index = i
+#         for j in range(i + 1, len(numbers)):
+#             if numbers[j] > numbers[min_index]:
+#                 min_index = j
+#         numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
+#     return numbers
+# numbers = [5, 3, 8, 1, 2]
+# selection_sort(numbers)
+# print(numbers)
 
