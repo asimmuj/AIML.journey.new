2938e3274b7a1bf2417b848b9ffdb1b0bc6e332e
asimmuj <asimmujtabahussain@gmail.com>
2026-09-08 20:49:21 +0530

    DAY-18

---
+++ DAY 18/miniproject.py
@@ -0,0 +1,19 @@
+def count_digits(n):
+    if n == 0:
+        return 0
+    return 1 + count_digits(n // 10)
+
+def sum_digits(n):
+    if n == 0:
+        return 0
+    return (n % 10) + sum_digits(n // 10)
+
+def reverse_number(n, result=0):
+    if n == 0:
+        return result
+    return reverse_number(n // 10, result * 10 + (n % 10))
+
+number = int(input("Enter a positive integer: "))
+print("Number of digits:", count_digits(number))
+print("Sum of digits:", sum_digits(number))
+print("Reverse:", reverse_number(number))
