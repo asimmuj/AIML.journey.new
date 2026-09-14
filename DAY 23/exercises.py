f5e68b05605befbf913cc5f4f83a8ce1d88eb27a
asimmuj <asimmujtabahussain@gmail.com>
2026-09-14 10:08:23 +0530

    DAY-23

---
+++ DAY 23/exercises.py
@@ -0,0 +1,35 @@
+# #exercise 1
+# stack = []
+# stack.append(5)#add 5 to the stack
+# stack.append(10)#add 10 to the stack
+# stack.append(15)#add 15
+# print(stack)
+# stack.pop()#removes the top which is 15 in this case
+# print(stack)
+# print(stack[-1])#top element which is 10 in this case
+
+#exercise 2
+# stack=[]
+# stack.append(10)
+# stack.append(20)
+# stack.append(30)
+# stack.append(40)
+# stack.append(50)
+# print(stack)
+# a=stack.pop()
+# b=stack.pop()
+# print("removed: ",a, b)
+# print(stack)
+# print(stack[-1])
+
+#execise 3
+stack=[]
+stack.append("mujtaba")
+stack.append("ali")
+stack.append("ahmed")
+stack.append("sara")
+print(stack)
+removed=stack.pop()
+print("removed: ",removed)
+print(stack)
+print(stack[-1])
