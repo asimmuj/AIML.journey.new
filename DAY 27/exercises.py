d95917dde024146a4469771f3dc23caae7fdbf11
asimmuj <asimmujtabahussain@gmail.com>
2026-09-20 09:47:56 +0530

    DAY-27

--- DAY 27/exercises.py
+++ DAY 27/exercises.py
@@ -1,8 +1,28 @@
-def example(numbers):
-    for number in numbers:
-        print(number)
+# def example(numbers):
+#     for number in numbers:
+#         print(number)
+# numbers = [10, 20, 30, 40, 50]
+# example(numbers)
 
+# def example(numbers):
+#     for i in numbers:
+#         for j in numbers:
+#             print(i, j)
+# numbers = [1, 2, 3, 4, 5]
+# example(numbers)
 
-numbers = [10, 20, 30, 40, 50]
+# def example(numbers):
+#     print(numbers[0])
+#     for number in numbers:
+#         print(number)
+# numbers = [10, 20, 30, 40, 50]
+# example(numbers)
 
-example(numbers)
+# def example(numbers):
+#     for i in numbers:
+#         for j in numbers:
+#             print(i, j)
+#     for number in numbers:
+#         print(number)
+# numbers = [1, 2, 3, 4, 5]
+# example(numbers)
