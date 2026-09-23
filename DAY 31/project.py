18854f98777ede77552b040e71ef623150871865
asimmuj <asimmujtabahussain@gmail.com>
2026-09-23 19:20:54 +0530

    DAY-31

---
+++ DAY 31/project.py
@@ -0,0 +1,14 @@
+import numpy as np
+data = np.array([
+    [80, 75, 60, 85],
+    [90, 88, 72, 91],
+    [70, 65, 80, 78],
+    [95, 92, 89, 94]
+])
+print("Student marks:")
+print(data)
+print("\nNumber of dimensions:", data.ndim)
+print("Dataset shape:", data.shape)
+print("Total values:", data.size)
+print("Number of students:", data.shape[0])
+print("Number of subjects:", data.shape[1])
