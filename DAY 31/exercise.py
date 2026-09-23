b907fc19256e3d3e23e91ccd3d0ade9299534fbe
asimmuj <asimmujtabahussain@gmail.com>
2026-09-23 19:03:56 +0530

    DAY-31

---
+++ DAY 31/exercise.py
@@ -0,0 +1,39 @@
+# #exercise 1
+# import numpy as np
+# numbers = np.array([10, 20, 30, 40, 50])
+# print(numbers)
+# print(numbers.ndim)
+# print(numbers.shape)
+# print(numbers.size)
+
+# #exercise 2
+# import numpy as np
+# array=np.array([
+#  [85, 90, 78],
+#  [72, 88, 91],
+#  [95, 89, 94],
+#  [60, 75, 70]
+# ])
+# print(array)
+# print(array.ndim)
+# print(array.shape)
+# print(array.size)
+
+#exercise 3
+import numpy as np
+array=np.array(
+    [
+    [
+        [1, 2, 3],
+        [4, 5, 6]
+    ],
+    [
+        [7, 8, 9],
+        [10, 11, 12]
+    ]
+]
+)
+print(array)
+print(array.ndim)
+print(array.shape)
+print(array.size)
