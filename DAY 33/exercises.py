fb31837232beadcdccc776c86b0a59638eeb6a87
asimmuj <asimmujtabahussain@gmail.com>
2026-09-25 15:17:29 +0530

    DAY-33

---
+++ DAY 33/exercises.py
@@ -0,0 +1,28 @@
+# #exercise 1
+# import numpy as np
+# numbers = np.array([10, 20, 30, 40, 50])
+# print(numbers+10)
+# print(numbers*3)
+# print(numbers-5)
+# print(numbers//2)
+
+#exercise 2
+import numpy as np
+marks = np.array([
+    [70, 80, 90],
+    [60, 75, 85],
+    [90, 88, 95]
+])
+add=np.array([5, 10, 3])
+print(marks+add)
+
+#exercise 3
+# import numpy as np
+# temperatures = np.array([
+#     [30, 32, 35],
+#     [28, 31, 33]
+# ])
+# F=temperatures * 9/5 + 32
+# print(F)
+
+#exercise 4
